f5958244368c3a1839a72aa3b1593c3db9451760
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-07 18:02:08 +0200

    ht10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetch(`https://restcountries.com/v3.1/name/germany?fields=name,capital,population,flags,languages`)
+//     .then(r => r.json())
+//     .then(r => {
+//         console.log(r[0]);
+//         const { flags, name, capital, languages, population } = r[0];
+//         console.log(flags, name, capital, languages, population);
+//     });
+
+fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages')
+    .then(r => r.json())
+    .then(console.log);
